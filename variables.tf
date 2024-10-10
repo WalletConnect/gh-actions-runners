8e66e40bf4e176f567472710cd0c60277b9601fb
Chris Smith <chris@walletconnect.com>
2024-10-10 13:33:02 -0400

    chore: add docs

--- variables.tf
+++ variables.tf
@@ -1,5 +1,6 @@
 variable "webhook_runners_github_pat" {
   type = string
+  description = "A GitHub PAT that's able to register runners on the repository. Should be admin access to the repo as described here:https://docs.github.com/en/rest/actions/self-hosted-runners?apiVersion=2022-11-28#create-a-registration-token-for-a-repository"
 }
 
 variable "webhook_runners_github_webhook_secret" {
